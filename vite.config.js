d2d700eaa25da91ba5b625c888450be329164bef
Rochedo098 <75433591+Rochedo098@users.noreply.github.com>
2022-05-31 20:39:27 -0300

    Fix dev Vite command

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,5 @@ export default defineConfig({
         alias: {
           '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-    },
-    build: {
-        rollupOptions: {
-            input: {
-                app: resolve('./public/index.html'),
-            }
-        }
     }
 })
