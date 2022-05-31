6b7887824e9aa29583d37b4277c942ab4769ba08
Rochedo098 <75433591+Rochedo098@users.noreply.github.com>
2022-05-31 12:24:53 -0300

    Simple changes for build and dev work

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
 <template>
-    
+  <RouterView />
 </template>
 
-<style scoped>
+<style>
+    @import '@/assets/theme.css';
 
+    html {
+        width: 100%;
+        height: 100%;
+
+        left: 0;
+        top: 0;
+        
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        background: var(--background);
+        color: var(--font);
+    }
 </style>
--- src/windows/Main.vue
+++ src/windows/Main.vue
@@ -1,7 +1,26 @@
 <template>
-    
+    <section id="main">
+
+    </section>
 </template>
 
 <style scoped>
+    section#main {
+        /* SIZE */
+        width: 100%;
+        height: 100%;
 
+        /* GRID */
+        display: grid;
+        grid-template-areas:
+        'n001 s001 s001 s001 s001 s001 s001'
+        'n001 s001 s001 s001 s001 s001 s001'
+        'n001 s001 s001 s001 s001 s001 s001'
+        'n001 s001 s001 s001 s001 s001 s001'
+        'n001 s001 s001 s001 s001 s001 s001';
+        /*
+            n001 = navbar
+            s001 = space01
+        */
+    }
 </style>
